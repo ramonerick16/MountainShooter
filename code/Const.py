4b156285e8552c81bad322dbeeb80e7365a4004d
ErickPJ <ramonerick16@hotmail.com>
2026-02-17 12:02:19 -0300

    Creating asset folder Inserting all assets Creating the menu: backgrund, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+from turtledemo.nim import COLOR
+
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
+#C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE',
+                'EXIT')
+
+#W
+WIN_WIDTH =576
+WIN_HEIGHT = 324
--- code/Game.py
+++ code/Game.py
@@ -2,22 +2,19 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH , WIN_HEIGHT
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT ))
 
-    def run(self ):
+    def run(self):
+        menu = Menu(self.window)  # Cria o menu uma única vez
+        menu.run()  # Inicia a lógica do menu
 
         while True:
-            menu = Menu(self.window)
-            menu.run()
-            pass
+            pass  # O 'pass' deve estar com 8 espaços (ou 2 tabs) de recuo
 
-        # Check for all events
-        # for event in pygame.event.get():
-        #    if event.type == pygame.QUIT:
-        #       pygame.quit()  # Close  Windows
-        #      quit()  # end pygame
+''
